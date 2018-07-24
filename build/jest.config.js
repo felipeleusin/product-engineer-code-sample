50708c926d95f8a5225c21073e3c4eb73edd22a6
Felipe Amorim <felipe.leusin@gmail.com>
2018-07-24 19:32:05 +0000

    (feat) bootstrapped project structure

---
+++ build/jest.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// import Enzyme from 'enzyme'
+// import Adapter from 'enzyme-adapter-react-16'
+import 'jest-styled-components'
+import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
+
+// Enzyme.configure({ adapter: new Adapter() })
